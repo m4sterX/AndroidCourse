09c8004906719b06377eba5988e0ebabec63af51
m4sterX <3119009@gmail.com>
2020-05-02 17:28:30 +0300

    ht7 done

---
+++ hometask7/src/main/java/com/example/hometask7/DAO/ContactDao.java
@@ -0,0 +1,35 @@
+package com.example.hometask7.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.hometask7.entity.Contact;
+
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+    @Query("Select * FROM contact")
+    List<Contact> getAll();
+
+    @Query("Select * FROM contact WHERE id = :id")
+    Contact getContactById(int id);
+
+    @Insert
+    void insert(Contact contact);
+
+    @Update
+    void update(Contact contact);
+
+    @Delete
+    void delete(Contact contact);
+
+    @Query("Select id FROM contact WHERE id >= 0")
+    List<Integer> getId();
+
+
+}
