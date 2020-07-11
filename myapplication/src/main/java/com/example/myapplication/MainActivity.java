0ea65742e54de7a800a686830a775d10e3d706a9
nkttrvts <3119009@gmail.com>
2020-07-11 12:21:56 +0300

    ht 2 is ready for check in

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        CustomView customview = new CustomView(this);
+        CustomView customview = new CustomView(getApplicationContext(), new CustomView.CustomListener() {
+            @Override
+            public void getXY(float x, float y) {
+                Toast.makeText(getApplicationContext(), "X = " + x + " Y = " + y, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         setContentView(customview);
+
     }
 }
